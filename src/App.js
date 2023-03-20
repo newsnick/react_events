7cbc98cfc7ca8c9e94896d41d41746a4ecbd1614
Antonio Lega <atlcontact8@gmail.com>
2023-03-20 20:46:29 +0100

    task: React fetch JSONplaceholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styles from './assets/Styles/App.css'
+import PostData from './Components/PostData/PostData'
 
 function App() {
+  const [postId, setPostId] = useState('')
+  const [post, setPost] = useState(null)
+
+  const handleChange = (event) => {
+    setPostId(event.target.value)
+  }
+
+  const handleClick = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => response.json())
+      //.then((data) => console.log(data))
+      .then((data) => setPost(data))
+
+      .catch((error) => console.error(error))
+    if (!postId || postId <= 0 || postId > 100) {
+      console.log('ID not valid')
+    } else {
+      console.log('ID valid')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label htmlFor="postId">Post ID:</label>
+        <input type="text" id="postId" value={postId} onChange={handleChange} />
+        <button onClick={handleClick}>Fetch Post</button>
+      </div>
+      {post && <PostData post={post} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+/////////////////////////////
+/////////////////////////////
+////////////////////////////
+/*
+/////events
+////////////////////////////
+//import './App.css'
+//import "./assets/styles/App.css";
+import { useState } from 'react'
+
+function App() {
+  const [value, setValue] = useState('')
+  const [value2, setValue2] = useState('')
+
+  function handleChange(event) {
+    setValue(event.target.value)
+  }
+
+  function handleChange2(event) {
+    setValue2(event.target.value)
+  }
+
+  function checkPassword(value) {
+    if (value) {
+      console.log('good')
+    } else {
+      console.log('wrong password')
+    }
+  }
+
+  return (
+    <div className="App">
+      <input type="number" value={value} onChange={handleChange} />
+      <input type="text" value={value2} onChange={handleChange2} />
+      <h3>
+        {+value} + {+value2} = {+value + +value2}
+      </h3>
+    </div>
+  )
+}
+
+export default App
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+/*   /*  function checkPassword(value) {
+    if (value) {
+      console.log('good')
+    } else {
+      console.log('wrong password')
+    }
+  } 
+
+  return (
+    <div className="App">
+      <input type="text" value={value} onChange={handleChange} />
+      <input type="text" value={value2} onChange={handleChange2} />
+      <h3>
+        {+value} + {+value2} = {+value} + {+value + +value2}
+      </h3>
+    </div>
+  )
+}
+
+export default App */
+
+/*////////////////onclick
+//import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [amount, setAmount] = useState(0)
+
+  function handleClick() {
+    setAmount(amount + 1)
+  }
+
+  return (
+    <div className="App">
+      <h3>You clicked {amount} times</h3>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  )
+}
+
+export default App
+*/
